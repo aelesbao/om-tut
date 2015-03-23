353add4f25392717f25f0221018682aa5edd90c2
Augusto Elesbão <aelesbao@gmail.com>
2015-03-23 18:36:13 -0300

    Add contacts

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -3,13 +3,34 @@
   (:require [figwheel.client :as fw]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :as async :refer [<!]]))
+            [cljs.core.async :as async :refer [<!]]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
 (println "Edits to this text should show up in your developer console.")
 
-;; define your app data so that it doesn't get over-written on reload
+(defn- parse-contact [contact-str]
+  (let [[first middle last :as parts] (string/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (string/replace middle "." ""))
+        c (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+              (== c 1) (assoc :middle-initial middle)
+              (>= c 2) (assoc :middle middle)))))
+
+(defn- add-contact [data owner]
+  (let [input (om/get-node owner "new-contact")]
+    (when-let [new-contact (-> input .-value parse-contact)]
+      (om/transact! data :contacts #(conj % new-contact))
+      (om/set-state! owner :text ""))))
+
+(defn handle-change [e owner {:keys [text]}]
+  (let [value (.. e -target -value)]
+    (if-not (re-find #"[0-9]" value)
+      (om/set-state! owner :text value)
+      (om/set-state! owner :text text))))
 
 (defn- display-name [{:keys [first last]}]
   (str first " " last))
@@ -20,13 +41,17 @@
     (render-state [_ {:keys [delete]}]
       (dom/li #js {:className "contact"}
               (dom/span nil (display-name contact))
-              (dom/button #js {:onClick (fn [_] (async/put! delete @contact))} "Delete")))))
+              (dom/button #js {:onClick (fn [e]
+                                          (async/put! delete @contact)
+                                          (.preventDefault e))}
+                          "Delete")))))
 
-(defn contacts-view [data owner]
+(defn- contacts-view [data owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:delete (async/chan)})
+      {:delete (async/chan)
+       :text   ""})
 
     om/IWillMount
     (will-mount [_]
@@ -38,12 +63,15 @@
                 (recur))))))
 
     om/IRenderState
-    (render-state [_ {:keys [delete]}]
+    (render-state [_ state]
       (dom/div nil
                (dom/h2 nil "Contact list")
                (apply dom/ul nil
-                      (om/build-all contact-view (:contacts data)
-                                    {:init-state {:delete delete}}))))))
+                      (om/build-all contact-view (:contacts data) {:init-state state}))
+               (dom/div nil
+                        (dom/input #js {:type "text" :ref "new-contact" :value (:text state)
+                                        :onChange #(handle-change % owner state)})
+                        (dom/button #js {:onClick #(add-contact data owner)} "Add contact"))))))
 
 (defonce app-state (atom {:contacts [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
                                      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
